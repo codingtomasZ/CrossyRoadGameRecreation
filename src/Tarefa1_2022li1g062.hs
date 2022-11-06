156a9ed82671716cd0da677c6db3fa9c3fba29cb
a104443 <a104443@alunos.uminho.pt>
2022-11-06 19:27:03 +0000

    Complemento do exe 2, ainda imcompleto, e rascunho do exe3

--- src/Tarefa1_2022li1g062.hs
+++ src/Tarefa1_2022li1g062.hs
@@ -13,23 +13,35 @@ import LI12223
 
 mapaValido :: Mapa -> Bool
 mapaValido (Mapa l []) = True
+
 mapaValido (Mapa l ((Relva,(h1:t1)):t2)) 
   |(elem Tronco (h1:t1) || elem Carro (h1:t1)) = False
   |otherwise = mapaValido (Mapa l t2)
+
 mapaValido (Mapa l (((Rio v),(h1:t1)):t2))
   |(elem Carro (h1:t1) || elem Arvore (h1:t1)) = False
   |otherwise = mapaValido (Mapa l t2)
+mapaValido (Mapa l (((Rio v),o):[])) = True
 mapaValido (Mapa l (((Rio v),o):((Rio v'),o'):t2))
    | v > 0 && v' > 0 = False
    | v < 0 && v' < 0 = False
-   |otherwise = True
+   |otherwise = mapaValido (Mapa l (((Rio v'),o'):t2))
+mapaValido (Mapa l (((Estrada v),o):((Rio v'),o'):t2)) = True
+mapaValido (Mapa l (((Relva),o):((Rio v'),o'):t2)) = True
+mapaValido (Mapa l (((Rio v),o):((Estrada v'),o'):t2)) = True
+mapaValido (Mapa l (((Rio v),o):((Relva),o'):t2)) = True
+
 mapaValido (Mapa l (((Estrada v),(h1:t1)):t2))
   |(elem Arvore (h1:t1) || elem Tronco (h1:t1)) = False
   |otherwise = mapaValido (Mapa l t2)
 
 
-mapaValido2 (Mapa l (((Rio v),o):((Rio v'),o'):t2))
-   | v > 0 && v' > 0 = False
-   | v < 0 && v' < 0 = False
-   |otherwise = True
- 
+
+{-mapaValido3 :: Mapa -> Bool
+mapaValido3 m = aux m 0
+
+aux :: Eq a => Mapa -> x -> Bool
+aux (Mapa l (((Rio v),[]):t2)) x = True 
+aux (Mapa l (((Rio v),(h1:t1)):t2)) x
+   | x == 5 = False
+   | h1 == Tronco = aux (Mapa l (((Rio v),(t1)):t2)) (x+1)-}
