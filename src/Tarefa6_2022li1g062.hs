18021f39b5ab91113d5405a8ba07df171bbda19f
Tomas Melo <tmelo949@gmail.com>
2022-11-21 10:15:06 +0000

    ABC

---
+++ src/Tarefa6_2022li1g062.hs
@@ -0,0 +1,14 @@
+{- |
+Module      : Tarefa4_2022li1g062
+Description : Determinar se o jogo terminou
+Copyright   : Tomas Henrique Alves Melo <a104529@alunos.uminho.pt>
+              José Diogo Azevedo Martins <a104443@alunos.uminho.pt>
+
+Módulo para a realização da Tarefa 6 do projeto de LI1 em 2022/2023.
+-}
+
+module Tarefa6_2022li1g062 where
+
+import LI12223
+
+import Test.HUnit 
