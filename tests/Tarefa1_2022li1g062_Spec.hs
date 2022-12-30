91e95d0de46254bb13f2cc5103d041681afe1d9a
Tomas Melo <tmelo949@gmail.com>
2022-12-30 12:30:41 +0000

    alterei os testes da 1 e faltam alguns testes da 5 depois de corrigirmos a deslizajogo meto os que faltam

--- tests/Tarefa1_2022li1g062_Spec.hs
+++ tests/Tarefa1_2022li1g062_Spec.hs
@@ -14,7 +14,7 @@ testsT2 :: Test
 testsT2 = TestLabel "Teste dos obstaculos" $ test ["Teste Arvore em Estrada" ~: False ~=?  mapaValido1 (Mapa 3 [(Estrada 2, [Arvore, Carro, Nenhum])])]
 
 testsT3 :: Test
-testsT3 = TestLabel "Teste dos obstaculos" $ test ["Teste Carro em Rio-ERRO, deve dar False" ~: True ~=?  mapaValido1 (Mapa 3 [(Rio 2, [Tronco, Carro, Nenhum])])]
+testsT3 = TestLabel "Teste dos obstaculos" $ test ["Teste Carro em Rio" ~: False ~=?  mapaValido1 (Mapa 3 [(Rio 2, [Tronco, Carro, Nenhum])])]
 
 testsT4 :: Test
 testsT4 = TestLabel "Teste dos obstaculos" $ test ["Teste Arvore em Rio" ~: False ~=? mapaValido1 (Mapa 3 [(Rio 2, [Arvore, Tronco, Nenhum])])]
@@ -29,19 +29,19 @@ testsT7 :: Test
 testsT7 = TestLabel "Teste dos rios" $ test ["Teste rios contiguos direçoes opostas" ~: False ~=? mapaValido2 (Mapa 2 [(Rio 2, [Tronco, Nenhum]), (Rio 2 , [Tronco, Nenhum])])]
 
 testsT8 :: Test 
-testsT8 = TestLabel "Teste dos obstaculos" $ test ["Teste max5 Tronco" ~: True ~=? mapaValido3 0 [Tronco , Tronco , Carro, Tronco , Tronco, Tronco, Tronco , Tronco]]
+testsT8 = TestLabel "Teste dos obstaculos" $ test ["Teste max5 Tronco" ~: False ~=? mapaValido3 (Mapa 5 [(Rio 1, [Tronco , Tronco , Nenhum , Tronco , Nenhum]), (Rio (1), [Nenhum , Tronco , Tronco , Tronco , Tronco, Tronco, Tronco])]) ]
 
 testsT9 :: Test
-testsT9 = TestLabel "Teste dos obstaculos" $ test ["Teste max5 comprimento Tronco" ~: False ~=? mapaValido3 0 [Tronco , Tronco , Carro, Tronco , Tronco, Tronco, Tronco , Tronco, Tronco]]
+testsT9 = TestLabel "Teste dos obstaculos" $ test ["Teste max5 Tronco" ~: True ~=? mapaValido3 (Mapa 6 [(Rio 1, [Tronco , Tronco , Nenhum , Tronco , Nenhum, Tronco]), (Rio (1), [Nenhum , Tronco , Tronco , Tronco , Tronco, Tronco])]) ]
 
 testsT10 :: Test
-testsT10 = TestLabel "Teste dos obstaculos" $ test ["Teste max3 comprimento Carro" ~: True ~=? mapaValido4 0 [Carro, Carro, Carro]]
+testsT10 = TestLabel "Teste dos obstaculos" $ test ["Teste max3 Carro" ~: True ~=? mapaValido4 (Mapa 5 [(Estrada 2 , [Carro , Carro, Carro, Nenhum, Nenhum]), (Estrada (-3)  , [Carro , Carro, Carro , Nenhum , Nenhum])]) ]
 
 testsT11 :: Test
-testsT11 = TestLabel "Teste dos obstaculos" $ test ["Teste max3 comprimento Carro-ERRO, deve dar False" ~: True ~=? mapaValido4 0 [Carro, Carro, Carro, Nenhum , Carro , Carro, Carro , Carro]]
+testsT11 = TestLabel "Teste dos obstaculos" $ test ["Teste max3 Carro" ~: False ~=? mapaValido4 (Mapa 5 [(Estrada 2 , [Carro , Carro, Carro, Carro, Nenhum]), (Estrada (-3)  , [Carro , Carro, Carro , Nenhum , Nenhum])])]
 
 testsT12 :: Test
-testsT12 = TestLabel "Teste dos obstaculos" $ test ["Teste max5 comprimento Tronco" ~: True ~=? mapaValido3 0 [Tronco , Nenhum, Tronco, Tronco, Tronco, Tronco , Tronco]]
+testsT12 = TestLabel "Teste dos obstaculos" $ test ["Teste max5 Tronco" ~: True ~=? mapaValido3 (Mapa 5 [(Rio 1, [Tronco , Tronco , Nenhum , Tronco , Nenhum]), (Rio (1), [Nenhum , Tronco , Tronco , Tronco , Nenhum])])]
 
 testsT13 :: Test
 testsT13 = TestLabel "Teste Nenhum" $ test ["Minimo um obstaculo Nenhum" ~: False ~=? mapaValido5 (Mapa 3 [(Rio 4 , [Tronco , Tronco ,Tronco])])]
@@ -50,10 +50,10 @@ testsT14 :: Test
 testsT14 = TestLabel "Teste Nenhum" $ test ["Minimo um obstaculo Nenhum" ~: True ~=? mapaValido5 (Mapa 3 [(Estrada 3 , [Carro, Nenhum , Nenhum])])]
 
 testsT15 :: Test
-testsT15 = TestLabel "Teste Nenhum" $ test ["Minimo um obstaculo Nenhum-ERRO, deve dar False" ~: True ~=? mapaValido5 (Mapa 2 [(Relva , [Arvore , Arvore])])]
+testsT15 = TestLabel "Teste Nenhum" $ test ["Minimo um obstaculo Nenhum" ~: False ~=? mapaValido5 (Mapa 2 [(Relva , [Arvore , Arvore])])]
 
 testsT16 :: Test 
-testsT16 = TestLabel "Teste largura e comprimento lista de obs" $ test ["Teste largura=comprimento lista de obs-ERRO, deve dar False" ~: True ~=? mapaValido6 (Mapa 3 [(Relva , [Arvore, Nenhum])])]
+testsT16 = TestLabel "Teste largura e comprimento lista de obs" $ test ["Teste largura=comprimento lista de obs" ~: False ~=? mapaValido6 (Mapa 3 [(Relva , [Arvore, Nenhum])])]
 
 testsT17 :: Test 
 testsT17 = TestLabel "Teste largura e comprimento lista de obs" $ test ["Teste largura=comprimento lista de obs" ~: False ~=? mapaValido6 (Mapa 3 [(Relva , [Arvore, Nenhum])])]
@@ -62,7 +62,7 @@ testsT18 :: Test
 testsT18 = TestLabel "Teste largura e comprimento lista de obs" $ test ["Teste largura=comprimento lista de obs" ~: True ~=? mapaValido6 (Mapa 2 [(Rio 1 , [Tronco , Nenhum])])]
 
 testsT19 :: Test 
-testsT19 = TestLabel "Teste rios" $ test ["Teste max4 Rio contiguos-ERRO, deve dar False" ~: True ~=? mapaValido7 (Mapa 2 [(Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) ])]
+testsT19 = TestLabel "Teste rios" $ test ["Teste max4 Rio contiguos" ~: False ~=? mapaValido7 (Mapa 2 [(Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) ])]
 
 testsT20 :: Test 
 testsT20 = TestLabel "Teste rios" $ test ["Teste max4 Rio contiguos" ~: True ~=? mapaValido7 (Mapa 2 [(Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum]) , (Rio 2 , [Tronco , Nenhum])])]
@@ -77,7 +77,7 @@ testsT23 :: Test
 testsT23 = TestLabel "Teste max5 Relva contiguas" $ test ["Relva max5" ~: False ~=? mapaValido7 (Mapa 2 [(Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) ])]
 
 testsT24 :: Test
-testsT24 = TestLabel "Teste max5 Relva contiguas" $ test ["Relva max5-ERRO, deve dar True" ~: False ~=? mapaValido7 (Mapa 2 [(Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) ])]
+testsT24 = TestLabel "Teste max5 Relva contiguas" $ test ["Relva max5" ~: True ~=? mapaValido7 (Mapa 2 [(Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) , (Relva , [Arvore , Nenhum]) ])]
 
 testsT25 :: Test
 testsT25 = TestLabel "Teste max4 Rio contiguos" $ test ["Terreno Relva seguido de 5 rios contiguos" ~: False ~=? mapaValido7 (Mapa 2 [(Relva , [Arvore, Nenhum]) , (Rio 3 , [Nenhum , Tronco]), (Rio 3 , [Nenhum , Tronco]), (Rio 3 , [Nenhum , Tronco]), (Rio 3 , [Nenhum , Tronco]), (Rio 3 , [Nenhum , Tronco]) ])]
